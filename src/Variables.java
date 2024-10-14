785e54ac2821a65a7c0408f49f1503ce9630eb90
CoolAid <96690746+CoolAid48@users.noreply.github.com>
2024-10-14 15:29:01 -0400

    Variables
    https://www.youtube.com/watch?v=xk4_1vDrzzo&t

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) throws Exception {
+
+        int x = 2;
+        int y = 4;
+        boolean z = true;
+        char symbol = '@';
+        String name = "-Aid";
+
+        System.out.println("Hello "+name);
+        System.out.println(symbol +"gmail.com");
+        System.out.println(x*y);
+        System.out.println(z);
+    }
+}
